9101fa046abed5bd4db9a3526f51974258145c09
ghost <diegobauto00@gmail.com>
2025-01-07 13:12:40 -0500

    feat: Cambio data por datos

--- models/renderizado.go
+++ models/renderizado.go
@@ -2,27 +2,27 @@ package models
 
 // Body que recibe la solicitud de /renderizar
 type Renderizado struct {
-	Css 		 string                 `json:"css"`
-	Plantilla_id string                 `json:"plantilla_id"`
-	Data         map[string]interface{} `json:"data"`
+	Css 		 string                  `json:"css"`
+	Plantilla_id string                  `json:"plantilla_id"`
+	Datos         map[string]interface{} `json:"datos"`
 }
 
 // Body que recibe la solicitud de /renderizarHTML
 type RenderizadoHTML struct {
-	Plantilla_id string                 `json:"plantilla_id"`
-	Data         map[string]interface{} `json:"data"`
+	Plantilla_id string                  `json:"plantilla_id"`
+	Datos         map[string]interface{} `json:"datos"`
 }
 
 // Body que recibe la solicitud de /renderizarPDF
 // Body que recibe la solicitud de /generar-pdf de PLANTILLAS_MID_RENDERIZADO_PLANTILLAS
 type RenderizadoPDF struct {
-	Css string 					 `json:"css"`
-	Html string                  `json:"html"`
-	Data map[string]interface{}  `json:"data"`
+	Css string 					  `json:"css"`
+	Html string                   `json:"html"`
+	Datos map[string]interface{}  `json:"datos"`
 }
 
 // Body que recibe la solicitud de /generar-html de PLANTILLAS_MID_RENDERIZADO_PLANTILLAS
 type BodyHTML struct {
-	Html string                 `json:"html"`
-	Data map[string]interface{} `json:"data"`
+	Html string                  `json:"html"`
+	Datos map[string]interface{} `json:"datos"`
 }
