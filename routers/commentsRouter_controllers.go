685d4b6b0a9e0121e0eedd3e90e101321286501b
CristianCGutierrezG <cris123.gut@gmail.com>
2026-03-20 16:47:40 -0500

    feat:ajuste de lineamientos en el mid udistrital/sisifo_documentacion#617

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,31 +7,31 @@ import (
 
 func init() {
 
-    beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoController"],
-        beego.ControllerComments{
-            Method: "Renderizar",
-            Router: "/renderizar",
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoController"],
+		beego.ControllerComments{
+			Method:           "Renderizar",
+			Router:           "/renderizar",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoHTMLController"] = append(beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoHTMLController"],
-        beego.ControllerComments{
-            Method: "RenderizarHTML",
-            Router: "/renderizar-html",
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoHTMLController"] = append(beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoHTMLController"],
+		beego.ControllerComments{
+			Method:           "RenderizarHTML",
+			Router:           "/renderizar-html",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoPDFController"] = append(beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoPDFController"],
-        beego.ControllerComments{
-            Method: "RenderizarPDF",
-            Router: "/renderizar-pdf",
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoPDFController"] = append(beego.GlobalControllerRouter["github.com/udistrital/plantillas_mid/controllers:RenderizadoPDFController"],
+		beego.ControllerComments{
+			Method:           "RenderizarPDF",
+			Router:           "/renderizar-pdf",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 }
